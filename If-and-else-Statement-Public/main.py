48d1213491e107b8e07f0eba4906f9424126a538
AmeyKuradeAK <ameykurade60@gmail.com>
2023-08-27 12:30:01 +0530

    If-and-else-Statement

---
+++ If-and-else-Statement-Public/main.py
@@ -0,0 +1,21 @@
+# Python Code for If Else Statement.
+r = 1002
+w = 3222
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
+
+r = 789
+w = 3222
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
+
+r = 300
+w = 233
+if r > w:
+  print("There are fewer white balls")
+else:
+  print("There are more white balls")
